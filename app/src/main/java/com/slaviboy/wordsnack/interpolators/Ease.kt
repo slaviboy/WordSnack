90563a815f1a94cbe0878b159e95ab4cc9248e8e
Stanislav Krasimirov Georgiev <slavi94slavi94@gmail.com>
2023-04-15 01:14:56 +0300

    Update easing methods

--- app/src/main/java/com/slaviboy/wordsnack/interpolators/Ease.kt
+++ app/src/main/java/com/slaviboy/wordsnack/interpolators/Ease.kt
@@ -6,33 +6,43 @@ package com.slaviboy.wordsnack.interpolators
  */
 sealed class Ease {
     object Linear : Ease()
+
     object QuadIn : Ease()
     object QuadOut : Ease()
     object QuadInOut : Ease()
+
     object CubicIn : Ease()
     object CubicOut : Ease()
     object CubicInOut : Ease()
+
     object QuartIn : Ease()
     object QuartOut : Ease()
     object QuartInOut : Ease()
+
     object QuintIn : Ease()
     object QuintOut : Ease()
     object QuintInOut : Ease()
+
     object SineIn : Ease()
     object SineOut : Ease()
     object SineInOut : Ease()
+
     object BackIn : Ease()
     object BackOut : Ease()
     object BackInOut : Ease()
+
     object CircIn : Ease()
     object CircOut : Ease()
     object CircInOut : Ease()
+
     object BounceIn : Ease()
     object BounceOut : Ease()
     object BounceInOut : Ease()
+
     object ElasticIn : Ease()
     object ElasticOut : Ease()
     object ElasticInOut : Ease()
+
     object EaseInExpo : Ease()
     object EaseOutExpo : Ease()
     object EaseInOutExpo : Ease()
