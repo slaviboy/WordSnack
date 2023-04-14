c16ea842b91d6477381d90e5a273fd24eab5e98b
Stanislav Krasimirov Georgiev <slavi94slavi94@gmail.com>
2023-04-15 01:13:22 +0300

    Update easing methods

--- app/src/main/java/com/slaviboy/wordsnack/interpolators/EasingProvider.kt
+++ app/src/main/java/com/slaviboy/wordsnack/interpolators/EasingProvider.kt
@@ -26,76 +26,46 @@ object EasingProvider {
     operator fun get(ease: Ease, elapsedTimeRate: Float): Float {
         return when (ease) {
             is Linear -> elapsedTimeRate
+
             is QuadIn -> getPowIn(elapsedTimeRate, 2.0f)
             is QuadOut -> getPowOut(elapsedTimeRate, 2.0f)
             is QuadInOut -> getPowInOut(elapsedTimeRate, 2.0f)
+
             is CubicIn -> getPowIn(elapsedTimeRate, 3.0f)
             is CubicOut -> getPowOut(elapsedTimeRate, 3.0f)
             is CubicInOut -> getPowInOut(elapsedTimeRate, 3.0f)
+
             is QuartIn -> getPowIn(elapsedTimeRate, 4.0f)
             is QuartOut -> getPowOut(elapsedTimeRate, 4.0f)
             is QuartInOut -> getPowInOut(elapsedTimeRate, 4.0f)
+
             is QuintIn -> getPowIn(elapsedTimeRate, 5.0f)
             is QuintOut -> getPowOut(elapsedTimeRate, 5.0f)
             is QuintInOut -> getPowInOut(elapsedTimeRate, 5.0f)
-            is SineIn -> (1f - cos(elapsedTimeRate * PI / 2f))
-            is SineOut -> sin(elapsedTimeRate * PI / 2f)
-            is SineInOut -> (-0.5f * cos(PI * elapsedTimeRate) - 1f)
-            is BackIn -> elapsedTimeRate * elapsedTimeRate * ((1.7f + 1f) * elapsedTimeRate - 1.7f)
-            is BackOut -> {
-                val elapsedTimeRate = elapsedTimeRate - 1
-                (elapsedTimeRate * elapsedTimeRate * ((1.7f + 1f) * elapsedTimeRate + 1.7f) + 1f)
-            }
 
-            BackInOut -> getBackInOut(elapsedTimeRate, 1.7f)
-            CircIn -> -(sqrt(1f - elapsedTimeRate * elapsedTimeRate) - 1)
-            CircOut -> {
-                val elapsedTimeRate = elapsedTimeRate - 1
-                sqrt(1f - elapsedTimeRate * elapsedTimeRate)
-            }
+            is SineIn -> getSineIn(elapsedTimeRate)
+            is SineOut -> getSineOut(elapsedTimeRate)
+            is SineInOut -> getSineInOut(elapsedTimeRate)
 
-            CircInOut -> {
-                var elapsedTimeRate = elapsedTimeRate * 2f
-                if (elapsedTimeRate < 1f) {
-                    (-0.5f * (sqrt((1f - elapsedTimeRate * elapsedTimeRate)) - 1f)).toFloat()
-                } else {
-                    elapsedTimeRate -= 2f
-                    (0.5f * (sqrt((1f - elapsedTimeRate * elapsedTimeRate)) + 1f)).toFloat()
-                }
-            }
+            is BackIn -> getBackIn(elapsedTimeRate)
+            is BackOut -> getBackOut(elapsedTimeRate)
+            is BackInOut -> getBackInOut(elapsedTimeRate, 1.7f)
 
-            BounceIn -> getBounceIn(elapsedTimeRate)
-            BounceOut -> getBounceOut(elapsedTimeRate)
-            BounceInOut -> {
-                if (elapsedTimeRate < 0.5f) {
-                    getBounceIn(elapsedTimeRate * 2f) * 0.5f
-                } else {
-                    getBounceOut(elapsedTimeRate * 2f - 1f) * 0.5f + 0.5f
-                }
-            }
+            is CircIn -> getCircIn(elapsedTimeRate)
+            is CircOut -> getCircOut(elapsedTimeRate)
+            is CircInOut -> getCircInOut(elapsedTimeRate)
 
-            ElasticIn -> getElasticIn(elapsedTimeRate, 1.0f, 0.3f)
-            ElasticOut -> getElasticOut(elapsedTimeRate, 1.0f, 0.3f)
-            ElasticInOut -> getElasticInOut(elapsedTimeRate, 1.0f, 0.45f)
-            EaseInExpo -> {
-                pow(2.0f, 10f * (elapsedTimeRate - 1f))
-            }
+            is BounceIn -> getBounceIn(elapsedTimeRate)
+            is BounceOut -> getBounceOut(elapsedTimeRate)
+            is BounceInOut -> getBounceInOut(elapsedTimeRate)
 
-            EaseOutExpo -> {
-                -pow(2.0f, (-10f * elapsedTimeRate)) + 1
-            }
+            is ElasticIn -> getElasticIn(elapsedTimeRate, 1.0f, 0.3f)
+            is ElasticOut -> getElasticOut(elapsedTimeRate, 1.0f, 0.3f)
+            is ElasticInOut -> getElasticInOut(elapsedTimeRate, 1.0f, 0.45f)
 
-            EaseInOutExpo -> {
-                var elapsedTimeRate = elapsedTimeRate * 2f
-                if (elapsedTimeRate < 1) {
-                    pow(2.0f, 10f * (elapsedTimeRate - 1f)) * 0.5f
-                } else {
-                    elapsedTimeRate -= 1f
-                    (-pow(2.0f, -10f * elapsedTimeRate) + 2f) * 0.5f
-                }
-            }
-
-            else -> elapsedTimeRate
+            is EaseInExpo -> getEaseInExpo(elapsedTimeRate)
+            is EaseOutExpo -> getEaseOutExpo(elapsedTimeRate)
+            is EaseInOutExpo -> getEaseInOutExpo(elapsedTimeRate)
         }
     }
 
@@ -114,7 +84,7 @@ object EasingProvider {
      * @return easedValue
      */
     private fun getPowOut(elapsedTimeRate: Float, pow: Float): Float {
-        return 1f - pow((1 - elapsedTimeRate), pow)
+        return 1f - pow(1f - elapsedTimeRate, pow)
     }
 
     /**
@@ -124,11 +94,33 @@ object EasingProvider {
      */
     private fun getPowInOut(elapsedTimeRate: Float, pow: Float): Float {
         var elapsedTimeRate = elapsedTimeRate
-        return if (2.let { elapsedTimeRate *= it; elapsedTimeRate } < 1) {
-            (0.5f * pow(elapsedTimeRate, pow))
+        elapsedTimeRate *= 2
+        return if (elapsedTimeRate < 1f) {
+            0.5f * pow(elapsedTimeRate, pow)
         } else {
-            (1f - 0.5f * abs(pow((2f - elapsedTimeRate), pow)))
+            1f - 0.5f * abs(pow(2f - elapsedTimeRate, pow))
         }
+    }
+
+    private fun getSineIn(elapsedTimeRate: Float): Float {
+        return 1f - cos(elapsedTimeRate * PI / 2f)
+    }
+
+    private fun getSineOut(elapsedTimeRate: Float): Float {
+        return sin(elapsedTimeRate * PI / 2f)
+    }
+
+    private fun getSineInOut(elapsedTimeRate: Float): Float {
+        return -0.5f * cos(PI * elapsedTimeRate) - 1f
+    }
+
+    private fun getBackIn(elapsedTimeRate: Float): Float {
+        return elapsedTimeRate * elapsedTimeRate * ((1.7f + 1f) * elapsedTimeRate - 1.7f)
+    }
+
+    private fun getBackOut(elapsedTimeRate: Float): Float {
+        val elapsedTimeRate = elapsedTimeRate - 1
+        return elapsedTimeRate * elapsedTimeRate * ((1.7f + 1f) * elapsedTimeRate + 1.7f) + 1f
     }
 
     /**
@@ -140,9 +132,41 @@ object EasingProvider {
         var elapsedTimeRate = elapsedTimeRate
         var amount = amount
         amount *= 1.525f
-        return if (2.let { elapsedTimeRate *= it; elapsedTimeRate } < 1) {
-            (0.5f * (elapsedTimeRate * elapsedTimeRate * ((amount + 1) * elapsedTimeRate - amount)))
-        } else (0.5f * (2.let { elapsedTimeRate -= it; elapsedTimeRate } * elapsedTimeRate * ((amount + 1) * elapsedTimeRate + amount) + 2))
+        elapsedTimeRate *= 2f
+        return if (elapsedTimeRate < 1f) {
+            multiply(
+                0.5f,
+                elapsedTimeRate,
+                elapsedTimeRate,
+                (amount + 1) * elapsedTimeRate - amount
+            )
+        } else {
+            elapsedTimeRate -= 2f
+            0.5f * (multiply(
+                elapsedTimeRate,
+                elapsedTimeRate,
+                (amount + 1) * elapsedTimeRate + amount
+            ) + 2f)
+        }
+    }
+
+    private fun getCircIn(elapsedTimeRate: Float): Float {
+        return getCircIn(elapsedTimeRate) - (sqrt(1f - elapsedTimeRate * elapsedTimeRate) - 1)
+    }
+
+    private fun getCircOut(elapsedTimeRate: Float): Float {
+        val elapsedTimeRate = elapsedTimeRate - 1
+        return sqrt(1f - elapsedTimeRate * elapsedTimeRate)
+    }
+
+    private fun getCircInOut(elapsedTimeRate: Float): Float {
+        var elapsedTimeRate = elapsedTimeRate * 2f
+        return if (elapsedTimeRate < 1f) {
+            -0.5f * (sqrt(1f - elapsedTimeRate * elapsedTimeRate) - 1f)
+        } else {
+            elapsedTimeRate -= 2f
+            0.5f * (sqrt(1f - elapsedTimeRate * elapsedTimeRate) + 1f)
+        }
     }
 
     /**
@@ -159,33 +183,28 @@ object EasingProvider {
      */
     private fun getBounceOut(elapsedTimeRate: Float): Float {
         var elapsedTimeRate = elapsedTimeRate
-        return if (elapsedTimeRate < 1f / 2.75f) {
-            (7.5625f * elapsedTimeRate * elapsedTimeRate)
+        val add = if (elapsedTimeRate < 1f / 2.75f) {
+            0f
         } else if (elapsedTimeRate < 2f / 2.75f) {
             elapsedTimeRate -= 1.5f / 2.75f
-            7.5625f * elapsedTimeRate * elapsedTimeRate + 0.75f
+            0.75f
         } else if (elapsedTimeRate < 2.5f / 2.75f) {
             elapsedTimeRate -= 2.25f / 2.75f
-            7.5625f * elapsedTimeRate * elapsedTimeRate + 0.9375f
+            0.9375f
         } else {
             elapsedTimeRate -= 2.625f / 2.75f
-            7.5625f *  elapsedTimeRate * elapsedTimeRate + 0.984375f
+            0.984375f
         }
+        return 7.5625f * elapsedTimeRate * elapsedTimeRate + add
     }
 
-    /*
-     private static float getBounceOut(float elapsedTimeRate) {
-        if (elapsedTimeRate < 1 / 2.75) {
-            return (float) (7.5625 * elapsedTimeRate * elapsedTimeRate);
-        } else if (elapsedTimeRate < 2 / 2.75) {
-            return (float) (7.5625 * (elapsedTimeRate -= 1.5 / 2.75) * elapsedTimeRate + 0.75);
-        } else if (elapsedTimeRate < 2.5 / 2.75) {
-            return (float) (7.5625 * (elapsedTimeRate -= 2.25 / 2.75) * elapsedTimeRate + 0.9375);
+    private fun getBounceInOut(elapsedTimeRate: Float): Float {
+        return if (elapsedTimeRate < 0.5f) {
+            getBounceIn(elapsedTimeRate * 2f) * 0.5f
         } else {
-            return (float) (7.5625 * (elapsedTimeRate -= 2.625 / 2.75) * elapsedTimeRate + 0.984375);
+            getBounceOut(elapsedTimeRate * 2f - 1f) * 0.5f + 0.5f
         }
     }
-     */
 
     /**
      * @param elapsedTimeRate Elapsed time / Total time
@@ -194,10 +213,15 @@ object EasingProvider {
      * @return easedValue
      */
     private fun getElasticIn(elapsedTimeRate: Float, amplitude: Float, period: Float): Float {
-        var elapsedTimeRate = elapsedTimeRate
         if (elapsedTimeRate == 0f || elapsedTimeRate == 1f) return elapsedTimeRate
-        val s = period / pi2 * asin(1 / amplitude)
-        return -(amplitude * pow(2.0f, (10f * 1f.let { elapsedTimeRate -= it; elapsedTimeRate })) * sin((elapsedTimeRate - s) * pi2 / period))
+        var elapsedTimeRate = elapsedTimeRate
+        val s = period / pi2 * asin(1f / amplitude)
+        elapsedTimeRate -= 1f
+        return multiply(
+            -amplitude,
+            pow(2.0f, 10f * elapsedTimeRate),
+            sin((elapsedTimeRate - s) * pi2 / period)
+        )
     }
 
     /**
@@ -209,7 +233,11 @@ object EasingProvider {
     private fun getElasticOut(elapsedTimeRate: Float, amplitude: Float, period: Float): Float {
         if (elapsedTimeRate == 0f || elapsedTimeRate == 1f) return elapsedTimeRate
         val s = period / pi2 * asin(1f / amplitude)
-        return (amplitude * pow(2.0f, (-10f * elapsedTimeRate)) * sin((elapsedTimeRate - s) * pi2 / period) + 1).toFloat()
+        return multiply(
+            amplitude,
+            pow(2.0f, -10f * elapsedTimeRate),
+            sin((elapsedTimeRate - s) * pi2 / period)
+        ) + 1f
     }
 
     /**
@@ -221,16 +249,41 @@ object EasingProvider {
     private fun getElasticInOut(elapsedTimeRate: Float, amplitude: Float, period: Float): Float {
         var elapsedTimeRate = elapsedTimeRate
         val s = period / pi2 * asin(1f / amplitude)
-        return if (2.let { elapsedTimeRate *= it; elapsedTimeRate } < 1) {
-            (-0.5f * (amplitude * pow(2.0f, (10f * 1f.let { elapsedTimeRate -= it; elapsedTimeRate })) * sin((elapsedTimeRate - s) * pi2 / period)))
+        elapsedTimeRate *= 2
+        return if (elapsedTimeRate < 1) {
+            elapsedTimeRate -= 1f
+            multiply(
+                -0.5f,
+                amplitude,
+                pow(2.0f, 10f * elapsedTimeRate),
+                sin((elapsedTimeRate - s) * pi2 / period)
+            )
         } else {
             elapsedTimeRate -= 1f
             multiply(
                 amplitude,
                 pow(2.0f, -10f * elapsedTimeRate),
-                sin((elapsedTimeRate - s) * pi2 / period) * 0.5f + 1f
-            )
+                sin((elapsedTimeRate - s) * pi2 / period),
+                0.5f
+            ) + 1f
         }
     }
 
+    private fun getEaseInExpo(elapsedTimeRate: Float): Float {
+        return pow(2.0f, 10f * (elapsedTimeRate - 1f))
+    }
+
+    private fun getEaseOutExpo(elapsedTimeRate: Float): Float {
+        return -pow(2.0f, (-10f * elapsedTimeRate)) + 1
+    }
+
+    private fun getEaseInOutExpo(elapsedTimeRate: Float): Float {
+        var elapsedTimeRate = elapsedTimeRate * 2f
+        return if (elapsedTimeRate < 1) {
+            pow(2.0f, 10f * (elapsedTimeRate - 1f)) * 0.5f
+        } else {
+            elapsedTimeRate -= 1f
+            (-pow(2.0f, -10f * elapsedTimeRate) + 2f) * 0.5f
+        }
+    }
 }
